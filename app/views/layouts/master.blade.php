00bd151792eea867289af8790ca3348451094e1b
dhwaneetbhatt <dhwaneetbhatt@gmail.com>
2019-10-16 15:25:38 +0530

    Updating versions of nginx, PHP, front-end libs and TLS

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   {{ HTML::script('https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js') }}
-  {{ HTML::style('https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css') }}
+  {{ HTML::style('https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css') }}
   {{ HTML::style('css/main.css') }}
   {{ HTML::script('js/newrelic.min.js') }}
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -13,7 +13,7 @@
   @include('layouts/header')
   @yield('page')
 </body>
-  {{ HTML::script('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js') }}
+  {{ HTML::script('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js') }}
   {{ HTML::script('https://cdnjs.cloudflare.com/ajax/libs/jquery-datetimepicker/2.5.20/jquery.datetimepicker.min.js') }}
   {{ HTML::style('https://cdnjs.cloudflare.com/ajax/libs/jquery-datetimepicker/2.5.20/jquery.datetimepicker.min.css') }}
   {{ HTML::script('js/main.js') }}
--- deploy/app.dockerfile
+++ deploy/app.dockerfile
@@ -1,4 +1,4 @@
-FROM php:5.6.31-fpm-alpine
+FROM php:5.6.40-fpm-alpine
 
 RUN apk update \
 && apk add \    
